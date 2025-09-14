3a6025a4c4873041293b1b900ecb7b9dff21014b
N1599 <nikhilsharma1599@gmail.com>
2025-09-15 00:16:39 +0530

    Added Bitcoin DAG to dags folder

---
+++ dags/Bitcoin_DAG.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import papermill as pm
+
+
+
+default_args = {
+    "start_date" : datetime.now(),
+    "email": ["nikhilsharma1599@gmail.com"],
+    "email_on_failure": True,
+    "email_on_success": True
+}
+
+def run_bitcoin_notebook():
+     input_path = "/opt/airflow/dags/Apache_Spark_Mini_Project.ipynb"
+     output_path = "/opt/airflow/dags/Bitcoin_Analysis_output.ipynb"
+
+     pm.execute_notebook(
+          input_path,
+          output_path,
+     )
+
+
+with DAG(
+        "Bitcoin_Price_Analysis_Job",
+        schedule_interval = None,
+        default_args = default_args,
+        catchup=False,
+        tags = ["bitcoin"]
+
+) as dag:
+    
+    run_task = PythonOperator(
+        task_id = "run_bitcoin_notebook" , 
+        python_callable = run_bitcoin_notebook,
+    )
+
+    
